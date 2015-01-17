f19b1d4ac57bc29a7fe2adf910e381bc2f305e0d
Adam Sosnowski <sosnowskia@gmail.com>
2015-01-17 14:10:30 +0100

    Non-recursive variant<class... Types>.

--- type_list.hpp
+++ type_list.hpp
@@ -1,23 +1,102 @@
 #ifndef TYPE_LIST_HPP
-# define VARIADIC_HPP
+# define TYPE_LIST_HPP
+
+
+#include <type_traits>
+
+
 
 namespace type_list_ {
 
-// position of a type in a type list
-template<class Element, class Head, class... Tail>
-struct position
-{
-  static constexpr size_t value = 1 + position<Element, Tail...>::value;
-};
+  // position of a type in a type list
+  template<class Element, class Head, class... Tail>
+  struct position {
+    static constexpr size_t value = 1 + position<Element, Tail...>::value;
+  };
 
+  template<class Element, class... Tail>
+  struct position<Element, Element, Tail...> {
+    static constexpr size_t value = 0;
+  };
 
-template<class Element, class... Tail>
-struct position<Element, Element, Tail...>
-{
-  static constexpr size_t value = 0;
-};
+  
+  // foldl
+  template<class Init, template<class,class> class Op, class... List>
+  struct foldl;
+
+  template<class Init, template<class,class> class Op, class Head, class... Tail>
+  struct foldl<Init, Op, Head, Tail...> {
+    using type = typename foldl<typename Op<Init, Head>::type, Op, Tail...>::type; 
+  };
+
+  template<class Init, template<class,class> class Op>
+  struct foldl<Init, Op> {
+    using type = Init;
+  };
 
 } /* namespace type_list_ */
+
+
+// is_elem
+template<class Element, class... List>
+struct is_elem;
+
+template<class Element, class Head, class... Tail>
+struct is_elem<Element, Head, Tail...> {
+  using type = typename is_elem<Element, Tail...>::type;
+};
+
+template<class Element, class... Tail>
+struct is_elem<Element, Element, Tail...> {
+  using type = std::true_type;
+};
+
+template<class Element>
+struct is_elem<Element> {
+  using type = std::false_type;
+};
+ 
+
+// both_true -- &&
+template<class, class>
+struct both_true;
+
+template<>
+struct both_true<std::true_type, std::true_type>
+{
+  using type = std::true_type;
+};
+
+template<bool A, bool B>
+struct both_true<
+    std::integral_constant<bool, A>
+  , std::integral_constant<bool, B>>
+{
+  using type = std::false_type;
+};
+
+
+
+// either_true -- ||
+template<class, class>
+struct either_true;
+
+template<>
+struct either_true<
+    std::false_type
+  , std::false_type>
+{
+  using type = std::false_type;
+};
+
+template<bool A, bool B>
+struct either_true<
+    std::integral_constant<bool, A>
+  , std::integral_constant<bool, B>>
+{
+  using type = std::true_type;
+};
+
 
 
 // type_list
@@ -37,6 +116,7 @@ struct type_list<>
 };
 
 
+
 template<class Head, class... Tail>
 struct type_list<Head, Tail...>
 {
@@ -53,9 +133,55 @@ struct type_list<Head, Tail...>
   using append = type_list<Head, Tail..., Ts...>;
 
   using reverse = typename tail::reverse::template append<head>::template instantiate<type_list>;
+
+  template<template<class> class F>
+  using map = type_list<F<Head>, F<Tail>...>;
+
+  template<template<class,class> class Op>
+  using fold = typename type_list_::foldl<Head, Op, Tail...>::type;
+
+  template<class T>
+  using has_elem = typename is_elem<T, Head, Tail...>::type;
+
+  template<class... Ts>
+  using is_superset = typename type_list<Ts...>::template map<has_elem>::template fold<both_true>::type;
 };
 
 
 
+template<class C>
+struct size_of
+{
+  static constexpr size_t value = sizeof(C);
+};
 
-#endif /* VARIADIC_HPP */
+template<class C>
+struct align_of
+{
+  static constexpr size_t value = alignof(C);
+};
+
+
+
+template<class C, class D>
+struct greater_of
+{
+  using type = typename std::conditional<(C::value < D::value)
+      , D
+      , C>::type;
+};
+
+
+
+template<class T, T N, T M>
+struct greater_of<std::integral_constant<T,N>, std::integral_constant<T,M>>
+{
+  using type = typename std::conditional<(N < M)
+      , std::integral_constant<T,M>
+      , std::integral_constant<T,N>
+    >::type;
+};
+
+
+
+#endif /* TYPE_LIST_HPP */
--- variant.hpp
+++ variant.hpp
@@ -4,80 +4,119 @@
 #include <stdexcept>
 #include "type_list.hpp" 
 
+#include <typeinfo>
 
 
-// variant
-template<class... Types> 
-class variant;
 
-template<>
-class variant<> {
-  template<class... Ts>
-  friend class variant;
+struct boxed_type
+{
+  virtual ~boxed_type() {};
+};
 
-  void destroy_() {
-    // TODO: throw
+template<class T>
+struct boxed
+  : public boxed_type
+{
+  T value;
+
+  template<class... Args>
+  boxed(Args&&... args)
+    : value(std::forward<Args> (args)...)
+  {}
+
+  ~boxed() {}
+};
+
+
+template<class... Types>
+class variant
+{
+  static constexpr size_t max_value_size
+      = type_list<boxed<Types>...>::template map<size_of>::template fold<greater_of>::value;
+
+  static constexpr size_t strictest_value_alignment
+      = type_list<boxed<Types>...>::template map<align_of>::template fold<greater_of>::value;
+
+  alignas(strictest_value_alignment) char store_[max_value_size];
+
+  boxed_type const * ptr_() const {
+    return reinterpret_cast<boxed_type const *> (&store_);
   }
+
+  boxed_type * ptr_() {
+    return reinterpret_cast<boxed_type *> (&store_);
+  }
+
+  template<class T>
+  boxed<T> const & ref_() const {
+    auto ptr = dynamic_cast<boxed<T> const *>(ptr_());
+    if (! ptr)
+      throw std::bad_cast();
+
+    return *ptr;
+  }
+
+  template<class T>
+  boxed<T> & ref_() {
+    auto ptr = dynamic_cast<boxed<T> *>(ptr_());
+    if (! ptr)
+      throw std::bad_cast();
+
+    return *ptr;
+  }
+
 
 public:
-  template<class Type>
-  variant(Type &&) = delete;
+  // constructors / destructors
+  template<
+      class T
+    , class = typename std::enable_if<
+        type_list<Types...>::template has_elem<T>::value>::type>
+  variant(T &&a) {
+    new (ptr_()) boxed<T> (std::forward<T> (a));
+  }
 
-}; 
+  ~variant() { ptr_()->~boxed_type(); };
 
 
-template<class Type, class... Types>
-class variant<Type, Types...> {
+  // accessors
+  template<class T>
+  typename std::enable_if<
+      type_list<Types...>::template has_elem<T>::value
+    , T &>::type
+  get() {
+    return ref_<T>().value;
+  }
+
+  template<class T>
+  typename std::enable_if<
+      type_list<Types...>::template has_elem<T>::value
+    , T const &>::type
+  get() const {
+    return ref_<T>().value;
+  }
+
+
+  // mutators
   template<class... Ts>
-  friend class variant;
-
-  template<class T>
-  using type_tag = typename type_list<Type, Types...>::reverse::template position<T>;
-
-  union {
-    struct {
-      size_t tag;
-      Type value;
-    };
-
-    variant<Types...> other;
-  };
-
-  void destroy_() {
-    if (type_tag<Type>::value == tag)
-      value.~Type();
-    else other.destroy_();
-  }
-
-public:  
-  // ctors / dtors
-  variant(Type &&a)
-      : tag(type_tag<Type>::value)
-      , value(std::forward<Type> (a)) {}
-
-  template<class T>
-  variant(T &&a)
-    : other(std::forward<T>(a)) {}
-
-  ~variant() {
-    destroy_();
-  }
-
-  variant & operator=(Type a) {
-    tag = type_tag<Type>::value;
-    new (&value) Type(std::move(a));
-
+  typename std::enable_if<
+      type_list<Types...>::template is_superset<Ts...>::type::value
+    , variant &>::type
+  operator=(variant<Ts...> const &a) {
     return *this;
   }
-  
+
   template<class T>
-  variant & operator=(T &&a) {
-    destroy_();
-    other = std::forward<T> (a);
+  typename std::enable_if<
+      type_list<Types...>::template has_elem<T>::value
+    , variant &>::type
+  operator=(T &&a) {
+    ptr_()->~boxed_type();
+    new (ptr_()) boxed<T>(std::forward<T> (a));
+
     return *this;
   }
 };
-
 
 
 #endif /* VARIANT_HPP */
